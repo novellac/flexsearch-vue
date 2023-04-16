f71bf6cf694e1ab4a08d00cbbace4a251d8488f9
novellac <38117965+novellac@users.noreply.github.com>
2023-04-16 10:58:18 -0400

    chore: Add comments for new folks ;)

--- src/App.vue
+++ src/App.vue
@@ -39,13 +39,14 @@
 </template>
 
 <script setup>
-import Index from 'flexsearch'
+import Index from 'flexsearch' // This should not be deconstructed
 import { computed, onMounted, ref } from 'vue'
 
-const index = ref(new Index({}))
-const query = ref('')
-const results = ref([])
+const index = ref(new Index({})) // This may not need to be a ref, since the Index only gets built when the component is mounted and we don't need reactivity
+const query = ref('') // This is v-modeled to the search; it will change when you type into the search input
+const results = ref([]) // These results are from the index, not our data!
 
+// This data could come from anywhere, like an external API, but we've hardcoded stuff here to keep the example simple.
 const rawData = [
   { id: 1, title: 'Affenpinscher' },
   { id: 2, title: 'Akita' },
@@ -61,20 +62,25 @@ const rawData = [
   { id: 12, title: 'Whippet Two' }
 ]
 
+// flexsearch's index has an "add" method that we'll use for each of our rawData items.
 const buildIndex = () => {
   Object.values(rawData).forEach((item) => {
     index.value.add(item.id, item.title)
   })
 }
 
+// flexsearch's index has a "search" method we'll use to find item(s) with the word we typed into the search input.
 const doSearch = () => {
   results.value = index.value.search(query.value)
 }
 
+// This computed property filters all our data by the results - filters an array of objects by an array of strings.
 const filteredData = computed(() => {
   return rawData.filter((item) => results.value.includes(item.id))
 })
 
+// When the component mounts we'll get an index of all our content to work with.
+// If you were to allow users to, for example, remove items from the data set, you might need to re-index, or use flexsearch's index's remove method.
 onMounted(() => {
   buildIndex()
 })
